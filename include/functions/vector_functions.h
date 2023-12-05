2bb0234cc722b44e277ba72676e6f49134dd6366
antivlad63 <antivlad63@yandex.ru>
2023-12-05 23:46:35 +0400

    Heder now good

--- include/functions/vector_functions.h
+++ include/functions/vector_functions.h
@@ -1,24 +1,45 @@
 #pragma once
+
 #include <iostream>
+#include <random>
 
 namespace vlad {
 	template<typename T>
-	class Vector<T>{
+	class Vector {
 	private:
 		int size;
 		T* coordinates;
 	public:
 		Vector(int vector_size, T* points);
-		Vector(str random);
-		T operator[](int index const) const;
-		T operator[](int index const);
-		Vector operator+(const Vector first_vector, const Vector second_vector);
-		Vector operator-(const Vector first_vector, const Vector second_vector);
-		Vector operator*(const Vector first_vector, const Vector second_vector);
-		Vector operator*(const Vector first_vector, const float);
-		Vector operator/(const Vector first_vector, const float number);
+		Vector(int vector_size, T lowest, T highest);
 
-		std::ostream& operator<< (std::ostream& out, Vector vector);
-	
-	}
+		T operator[](int index);
 
+		bool operator==(Vector second_vector) const;
+
+		Vector operator+(Vector second_vector);
+		Vector operator-(Vector second_vector);
+		Vector operator*(Vector second_vector);
+
+		Vector operator*(float number);
+		friend Vector operator*(float& number, Vector first_vector);
+
+		Vector operator/(float number);
+
+		T vector_length();
+
+		friend std::ostream& operator<<(std::ostream& out, Vector vector);
+
+		inline static std::random_device random_device{};
+	};
+
+	//template <typename T>
+	//T triangle_square(Vector<T> first_vector, Vector<T> second_vector) {
+	//	Vector<T> third_vector = first_vector - second_vector;
+	//	T a = sqrt(first_vector * first_vector);
+	//	T b = sqrt(second_vector * second_vector);
+	//	T c = sqrt(third_vector * third_vector);
+	//	T p = (a + b + c) / 2;
+	//	return sqrt(p * (p - a) * (p - b) * (p - c));
+	//}
+}
